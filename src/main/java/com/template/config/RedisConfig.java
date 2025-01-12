cffd8b114889ef88c7fb29ac1dba9404da39ee49
heruiyuan27 <hery27@foxmail.com>
2025-01-12 17:23:27 +0800

    【redis】fastjson序列化

---
+++ src/main/java/com/template/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.template.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置类
+ *
+ * @author zhangzhixiang
+ * @date 2019年06月19日
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings(value = { "unchecked", "rawtypes" })
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
+
+        // key的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式
+        template.setValueSerializer(fastJsonRedisSerializer);
+        // hash的value序列化方式
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
--- src/main/java/com/template/controller/TestController.java
+++ src/main/java/com/template/controller/TestController.java
@@ -1,19 +1,30 @@
 package com.template.controller;
 
-import com.template.common.resp.ResultEnum;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.template.common.exception.BusinessException;
-import com.template.model.req.LongIdReq;
-import com.template.model.req.StringIdReq;
 import com.template.common.resp.CommonResponse;
 import com.template.common.resp.DataResponse;
+import com.template.common.resp.ResultEnum;
+import com.template.model.Student;
+import com.template.model.req.LongIdReq;
+import com.template.model.req.StringIdReq;
+import com.template.service.RedisCache;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 public class TestController {
+
+    @Resource
+    private RedisCache redisCache;
 
     @RequestMapping(value = "/getTest1", method = RequestMethod.GET)
     public CommonResponse getTest1(@RequestParam(value = "nickname", required = false) String name) {
@@ -37,6 +48,28 @@ public class TestController {
     public DataResponse<StringIdReq> postTest3(@RequestBody @Valid StringIdReq req) {
         log.info("QueryUserReq={}", req.getId());
         return DataResponse.success(req);
+    }
+
+    @RequestMapping(value = "/redisListPush", method = RequestMethod.GET)
+    public DataResponse<?> redisListPush() {
+        Student s1 = new Student("hhh", 10);
+        Student s2 = new Student("rrr", 11);
+        Student s3 = new Student("yyy", 12);
+        List<Student> list = new ArrayList<>();
+        list.add(s1);
+        list.add(s2);
+        list.add(s3);
+        long count = redisCache.setCacheList("list_key", list);
+        return DataResponse.success(count);
+    }
+
+    @RequestMapping(value = "/redisListGet", method = RequestMethod.GET)
+    public DataResponse<?> redisListGet() {
+        List<JSONObject> list = redisCache.getCacheList("list_key");
+        List<Student> result = list.stream()
+                .map(o -> JSON.toJavaObject(o, Student.class))
+                .collect(Collectors.toList());
+        return DataResponse.success(result);
     }
 
 }
