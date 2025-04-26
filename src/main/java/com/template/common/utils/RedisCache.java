ecbbc8fc183be26649c92855ff58bebea4418c33
heruiyuan27 <hery27@foxmail.com>
2025-04-26 21:57:07 +0800

    修订

--- src/main/java/com/template/common/utils/RedisCache.java
+++ src/main/java/com/template/common/utils/RedisCache.java
@@ -1,4 +1,4 @@
-package com.template.service;
+package com.template.common.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundSetOperations;
--- src/main/java/com/template/model/pojo/Student.java
+++ src/main/java/com/template/model/pojo/Student.java
@@ -1,4 +1,4 @@
-package com.template.model;
+package com.template.model.pojo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
