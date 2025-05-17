cacbcfba77c98866a74c744e46ee44e4ba749539
heruiyuan27 <hery27@foxmail.com>
2025-05-17 18:16:54 +0800

    修订

--- src/main/java/com/template/ws/HttpAuthWebSocketHandler.java
+++ src/main/java/com/template/ws/HttpAuthWebSocketHandler.java
@@ -17,21 +17,21 @@ public class HttpAuthWebSocketHandler extends TextWebSocketHandler {
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        log.warn("connected");
+        log.warn("已连接,session_id={},当前连接数={}", session.getId(), SESSION_MAP.size());
         SESSION_MAP.put(session.getId(), session);
-        session.sendMessage(new TextMessage("已连接,session_id=" + session.getId()));
+        session.sendMessage(new TextMessage("已连接,session_id=" + session.getId() + ",当前连接数=" + SESSION_MAP.size()));
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        log.warn("server 接收到 " + session.getId() + " 发送的 " + payload);
-        session.sendMessage(new TextMessage("server回复client:" + payload + "-" + session.getId()));
+        log.warn("server 接收到 {} 发送的 {}", session.getId(), payload);
+        session.sendMessage(new TextMessage("server已收到:" + payload ));
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        log.warn("disconnected");
+        log.warn("已断开,session_id={}当前连接数={}", session.getId(), SESSION_MAP.size());
         SESSION_MAP.remove(session.getId());
     }
 }
